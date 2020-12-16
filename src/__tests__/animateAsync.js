bfa2d9be8e347d0f91e8d95db9644683a14c6df1
Krivega Dmitriy <mr.krivega@gmail.com>
2020-12-16 18:16:58 +0300

    feat: add runAcync

---
+++ src/__tests__/animateAsync.js
@@ -0,0 +1,143 @@
+import delayPromise from 'promise-delay';
+import { REQUEST_ANIMATION_FRAME_TIMEOUT } from '../setupTests';
+import RequestAnimation from '../index';
+
+const delayTimeout = REQUEST_ANIMATION_FRAME_TIMEOUT / 4;
+
+/**
+ * Calc desiredCount calls
+ * @param {Object} params - Params for execution
+ * @param {number} params.timeoutRun - Timeout run frames
+ * @param {number} params.fps - Desired Frames per Second
+ * @param {number} params.countRuns - Count runs animate
+ * @returns {number} count
+ */
+const calcDesiredCountCalls = ({ timeoutRun, fps, countRuns }) => {
+  const timeElapsed = timeoutRun * countRuns;
+  const fpsInterval = 1000 / fps;
+
+  let count = timeElapsed / fpsInterval;
+
+  if (timeoutRun > fpsInterval) {
+    count = countRuns;
+  }
+
+  return Math.floor(count) + 1;
+};
+
+describe('RequestAnimation: animate async', () => {
+  let requestAnimation;
+  let mockFn;
+  let mockFn2;
+
+  beforeEach(() => {
+    jest.resetModules();
+    requestAnimation = new RequestAnimation();
+    mockFn = jest.fn().mockResolvedValue();
+    mockFn2 = jest.fn().mockResolvedValue();
+  });
+
+  it('animate: default fps=60', () => {
+    const countRuns = 2;
+    const fps = 60;
+
+    expect.assertions(1);
+
+    requestAnimation.runAsync(mockFn);
+
+    return delayPromise(REQUEST_ANIMATION_FRAME_TIMEOUT * countRuns + delayTimeout).then(() => {
+      requestAnimation.deactivate();
+
+      expect(mockFn.mock.calls.length).toBeLessThanOrEqual(
+        calcDesiredCountCalls({ fps, countRuns, timeoutRun: REQUEST_ANIMATION_FRAME_TIMEOUT })
+      );
+    });
+  });
+
+  it('animate: call 2 times fps=60', () => {
+    const countRuns = 3;
+    const fps = 60;
+
+    expect.assertions(2);
+
+    requestAnimation.runAsync(mockFn);
+    requestAnimation.runAsync(mockFn2);
+
+    return delayPromise(REQUEST_ANIMATION_FRAME_TIMEOUT * countRuns + delayTimeout).then(() => {
+      requestAnimation.deactivate();
+
+      expect(mockFn).toHaveBeenCalledTimes(0);
+      expect(mockFn2.mock.calls.length).toBeLessThanOrEqual(
+        calcDesiredCountCalls({ fps, countRuns, timeoutRun: REQUEST_ANIMATION_FRAME_TIMEOUT })
+      );
+    });
+  });
+
+  it('animate: fps=1', () => {
+    const countRuns = 1;
+    const fps = 1;
+
+    expect.assertions(1);
+
+    requestAnimation.runAsync(mockFn, fps);
+
+    return delayPromise(REQUEST_ANIMATION_FRAME_TIMEOUT * countRuns + delayTimeout).then(() => {
+      requestAnimation.deactivate();
+
+      expect(mockFn.mock.calls.length).toBeLessThanOrEqual(
+        calcDesiredCountCalls({ fps, countRuns, timeoutRun: REQUEST_ANIMATION_FRAME_TIMEOUT })
+      );
+    });
+  });
+
+  it('animate: less interval', () => {
+    const countRuns = 6;
+    const fps = countRuns - 2;
+
+    expect.assertions(1);
+
+    requestAnimation.runAsync(mockFn, fps);
+
+    return delayPromise(REQUEST_ANIMATION_FRAME_TIMEOUT * countRuns + delayTimeout).then(() => {
+      requestAnimation.deactivate();
+
+      expect(mockFn.mock.calls.length).toBeLessThanOrEqual(
+        calcDesiredCountCalls({ fps, countRuns, timeoutRun: REQUEST_ANIMATION_FRAME_TIMEOUT })
+      );
+    });
+  });
+
+  it('animate: less 1 fps', () => {
+    const countRuns = 2;
+    const fps = 1 / 4;
+
+    expect.assertions(1);
+
+    requestAnimation.runAsync(mockFn, fps);
+
+    return delayPromise(REQUEST_ANIMATION_FRAME_TIMEOUT * countRuns + delayTimeout).then(() => {
+      requestAnimation.deactivate();
+
+      expect(mockFn.mock.calls.length).toBeLessThanOrEqual(
+        calcDesiredCountCalls({ fps, countRuns, timeoutRun: REQUEST_ANIMATION_FRAME_TIMEOUT }) + 1 // 1 or 2
+      );
+    });
+  });
+
+  it('animate: more interval', () => {
+    const countRuns = 6;
+    const fps = countRuns + 2;
+
+    expect.assertions(1);
+
+    requestAnimation.runAsync(mockFn, fps);
+
+    return delayPromise(REQUEST_ANIMATION_FRAME_TIMEOUT * countRuns + delayTimeout).then(() => {
+      requestAnimation.deactivate();
+
+      expect(mockFn.mock.calls.length).toBeLessThanOrEqual(
+        calcDesiredCountCalls({ fps, countRuns, timeoutRun: REQUEST_ANIMATION_FRAME_TIMEOUT })
+      );
+    });
+  });
+});
--- src/index.js
+++ src/index.js
@@ -51,6 +51,49 @@ export default class RequestAnimation {
   }
 
   /**
+   * Resolve the animation loop calculates time elapsed since the last loop
+   * and only draws if your specified fps interval is achieved
+   *
+   * @param {function} animationFunc - Function for animation
+   * @param {string}   fps           - Frames per Second
+   *
+   * @returns {undefined}
+   */
+  async runAsync(animationFunc, fps = 60) {
+    const fpsInterval = 1000 / fps;
+
+    let delta = 0; // 0 for run for first, set Date.now() if no needs first run
+
+    /**
+     * Animate loop
+     *
+     * @param {number} timestamp - DOMHighResTimeStamp similar to the one returned by performance.now()
+     * indicating the point in time when requestAnimationFrame() starts to execute callback functions.
+     *
+     * @returns {undefined}
+     */
+    const animateLoop = async (timestamp) => {
+      // calc elapsed time since last loop
+      const elapsed = timestamp - delta;
+
+      // if enough time has elapsed, draw the next frame
+
+      if (elapsed > fpsInterval) {
+        // Get ready for next frame by setting delta=timestamp, but also adjust for your
+        // specified fpsInterval not being a multiple of RAF's interval (16.7ms)
+        delta = timestamp - (elapsed % fpsInterval);
+
+        await animationFunc();
+      }
+
+      this.request(animateLoop);
+    };
+
+    this.cancelRequest();
+    this.request(animateLoop);
+  }
+
+  /**
    * Request single call
    *
    * @param {function} animationFunc - Function for animation
